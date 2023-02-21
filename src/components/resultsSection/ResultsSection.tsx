4daf3c12f791cc1af79ccfe6d5d0d0f039f1c163
Radoslav <radoslav.varadinov@gmail.com>
2023-02-21 20:39:34 +0200

    Color palette change

--- src/components/resultsSection/ResultsSection.tsx
+++ src/components/resultsSection/ResultsSection.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 interface IResultsSectionProps {}
 
+import styles from "./ResultsSections.module.scss";
+
 const ResultsSection = (props: IResultsSectionProps) => {
-	return <div>ResultsSection</div>;
+	return (
+		<div className={styles.resultsSection}>
+			<p>Time is up!</p>
+		</div>
+	);
 };
 
 export default ResultsSection;
