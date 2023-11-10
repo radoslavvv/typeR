8b6f6c87e184937eb6f3578a87b5041cef872cd3
Radoslav <radoslav.varadinov@gmail.com>
2023-11-10 02:59:18 +0200

    Safari mobile version cursor fix;

--- src/components/writer/cursor/Cursor.tsx
+++ src/components/writer/cursor/Cursor.tsx
@@ -3,11 +3,18 @@ import useCursor from "../../../hooks/useCursor";
 const Cursor = () => {
   const { cursorTop, cursorLeft } = useCursor();
 
+  const isMobileDevice: boolean =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    );
+
   return (
-    <span
-      className="absolute h-6 rounded-full border border-solid border-lightBlue duration-300"
-      style={{ top: cursorTop, left: cursorLeft }}
-    ></span>
+    !isMobileDevice && (
+      <span
+        className="absolute h-6 rounded-full border border-solid border-lightBlue duration-300"
+        style={{ top: cursorTop, left: cursorLeft }}
+      ></span>
+    )
   );
 };
 
