17504e53500328678d38f1fa176bf40d6937cefc
Radoslav <radoslav.varadinov@gmail.com>
2023-11-10 02:54:32 +0200

    Mobile keyboard fix;

--- src/components/writer/mobileKeyboard/MobileKeyboard.tsx
+++ src/components/writer/mobileKeyboard/MobileKeyboard.tsx
@@ -2,7 +2,7 @@ const MobileKeyboard = () => {
   return (
     <input
       value={"Open Mobile Keyboard"}
-      className="text-#1b2028 mt-10 bg-transparent text-center text-lightBlue outline-none placeholder:font-bold placeholder:text-lightBlue"
+      className=" mt-10 bg-transparent text-center text-lightBlue outline-none placeholder:font-bold placeholder:text-lightBlue"
       placeholder="Open Mobile Keyboard"
     />
   );
